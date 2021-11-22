c99decfc497416a138768709de92c2f81691a183
cmiltiadou <cmiltiad@gmail.com>
2021-11-22 15:36:03 -0500

    app ready to be deployed to heroku

--- index.js
+++ index.js
@@ -119,6 +119,6 @@ app.get('/restaurant/:restName',  (req, res)=>{
 })
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("auth_practice running on port 3000")
 })
